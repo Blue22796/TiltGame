4dbb0868edda95703c026d74c78117c4bd648e0f
= <=>
2024-04-23 17:01:53 +0200

    Added Sample test

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
-﻿char[][] State = {  };
-int TargetX = 0;
-int TargetY = 0;
+﻿char[][] State = new char[][]{ 
+    new char[] { '#','#','.','.','.'}, 
+    new char[] { '.','o','#','.','.'}, 
+    new char[] { '.','.','o','.','.'}, 
+    new char[] { '.','.','.','.','.'}, 
+    new char[] { '#','#','#','.','.'} 
+};
+int TargetX = 4;
+int TargetY = 3;
 
-//Read input from file
+//Read input from file into State
 
 var Player = new Tilt_Game.Player();
 Player.Solve(State, TargetX, TargetY);

f88336bea646eb298b0dcdcfdde94e02a02c7a67
= <=>
2024-04-23 16:48:06 +0200

    Created project template

---
+++ Game.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tilt_Game
+{
+    public class Board
+    {
+
+        char[][] state;
+
+        public Board(char[][] state)
+        {
+            this.state = state;
+        }
+
+        
+        public Board(String state) { 
+            //Convert String to 2d array
+        }
+        //Modify array when tilting the board in different directions
+        #region Tilting Logic
+        public Board TiltRight() {
+            char[][] NewState = (char[][]) state.Clone();
+            return new Board(NewState);
+        }
+        public Board TilitDown() {
+            char[][] NewState = (char[][]) state.Clone();
+            return new Board(NewState);
+        }
+        public Board TiltUp() {
+            char[][] NewState = (char[][]) state.Clone();
+            return new Board(NewState);
+        }
+        public Board TiltLeft() {
+            char[][] NewState = (char[][]) state.Clone();
+            return new Board(NewState);
+        }
+        #endregion
+        //Returns String representation of board
+        public String toString() 
+        {
+            return null;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return this.toString().Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.toString().GetHashCode();
+        }
+    }
+
+    public class Player 
+    {
+        List<Board> Sequence = new List<Board>();
+        int TargetX;
+        int TargetY;
+        public List<Board> Solve(char[][] initialBoard, int targetX, int targetY)
+        {
+            //Generate Sequence starting with initialBoard
+            return Sequence;
+        }
+
+        public Boolean IsWinning(Board board) {
+            //Check if the current configuration is winning
+            return true;
+        }
+    }
+
+    public class Cameraman 
+    { 
+        //public GUIComponent Representation(Board board)
+        //{
+        //
+        //}
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿char[][] State = {  };
+int TargetX = 0;
+int TargetY = 0;
+
+//Read input from file
+
+var Player = new Tilt_Game.Player();
+Player.Solve(State, TargetX, TargetY);
